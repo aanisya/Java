42644e9cd83a833a087c9e0e34e4f5f8de9ca39b
aanisya <septianuranisya@gmail.com>
2024-03-21 22:01:00 +0700

    praktikum 1

---
+++ Belajar.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Belajar;
+
+/**
+ *
+ * @author USER
+ */
+public class Belajar {
+    public String x = "Pintar";
+    public String y = "Java";
+}
+
