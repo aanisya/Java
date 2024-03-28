ee2c84092667224b8ddb3af9e05906a1e8ce82ee
aanisya <septianuranisya@gmail.com>
2024-03-28 17:32:34 +0700

    praktikum 1

---
+++ LatScan4.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+import java.util.*;
+/**
+ *
+ * @author anisya
+ */
+public class LatScan4 {
+    public static void main(String[] args) {
+        int kd;
+        String nb="";
+        Scanner input=new Scanner(System.in);
+        System.out.print("Masukkan kode barang[1...3]:");
+        kd=input.nextInt();
+        
+        switch(kd)
+        {
+            case 1:
+                nb="Alat Olahraga";
+                break;
+            case 2:
+                nb="Alat Elektronik";
+                break;
+            default:
+                nb="Anda Salah Kode!!!!!!!!!!!!";
+                break;
+        }
+        System.out.println("\nNama Barang:]" +nb);
+    }
+    
+}
